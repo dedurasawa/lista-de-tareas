b300ca9e2576a72cacb630935d21290d8ad861ea
dedurasawa <113485097+dedurasawa@users.noreply.github.com>
2022-12-27 02:00:22 -0300

    mejorando lista de tareas, agregando fecha y hora

--- script.js
+++ script.js
@@ -1,40 +1,67 @@
-let entradaDatos = document.getElementById("textarea")
-let btnAgregar = document.getElementById("btnagregar")
-let tareaTexto = document.getElementById("tarea_text")
-let contenedor = document.getElementById("contenedor_listas")
-btnAgregar.addEventListener('click', guardarDato)
+let entradaDatos = document.getElementById("textarea");
+let btnAgregar = document.getElementById("btnagregar");
+let tareaTexto = document.getElementById("tarea_text");
+let contenedor = document.getElementById("contenedor_listas");
+let fecha = document.getElementById("fecha");
 
+btnAgregar.addEventListener('click', guardarDato);
 
+function formatoFecha() {
 
-
-
+    let arrayFecha = [];
+    arrayFecha.push(fecha.value);
+    let nuevaCadena = arrayFecha.toString();
+    let mejoraCadena = nuevaCadena.replace(/(-)/gm,"/");
+    let ultimaModificacion = mejoraCadena.replace(/(T)/gm," ");
+    return ultimaModificacion;
+}
 
 
 function guardarDato() {
-    let dato = entradaDatos.value
+    let dato = entradaDatos.value;
 
     let cardItem = document.createElement('li');
     cardItem.className = 'tarea';
+
     contenedor_listas.appendChild(cardItem);
 
     let revision = document.createElement('input');
+
     revision.type = 'checkbox';
     revision.className = "check";
+
     cardItem.appendChild(revision);
 
+    let separador = document.createElement('span');
+    cardItem.appendChild(separador);
+
+    let fecha = document.createElement('p');
+    fecha.innerText = formatoFecha();
+    fecha.className = 'fecha_texto';
+    separador.appendChild(fecha);
+
     let texto = document.createElement('p');
-    texto.innerText = dato
+//en la variable texto almacenamos la variable dato
+// (dicha variable se obtiene del textarea)
+    texto.innerText = dato;
     texto.className = 'tarea_text';
-    cardItem.appendChild(texto);
+
+    separador.appendChild(texto);
 
     let btnBorrar = document.createElement('button');
+
     btnBorrar.className = 'btn_borrar';
-    btnBorrar.innerText = 'borrar'
+    btnBorrar.innerText = 'borrar';
+
     cardItem.appendChild(btnBorrar);
+
     btnBorrar.addEventListener('click', function borrar() {
-        cardItem.remove()
+        cardItem.remove();
     })
 
-    contenedor.appendChild(cardItem)
+    contenedor.appendChild(cardItem);
 }
+
+
+// localStorage.setItem(key: DOMString, JSON.stringify(dato)
 
